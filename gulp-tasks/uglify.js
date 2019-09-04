e3cf2e8b7d1c23fe1a99f95059812075902936e7
Almaev Illia <chytvs777@mail.ru>
2019-09-05 02:22:13 +0300

    fix animate

--- gulp-tasks/uglify.js
+++ gulp-tasks/uglify.js
@@ -5,7 +5,7 @@ module.exports = function(options) {
     options.gulp.src([options.path.src.js + 'vendor/*.js', options.path.src.js + '*.js'])
     .pipe(options.plumber())
     .pipe(options.concat('app.js'))
-    //.pipe(options.uglify())
+    .pipe(options.uglify())
     .pipe(options.rename({ suffix : '.min' }))
     .pipe(options.gulp.dest(options.path.build.js));
   });
